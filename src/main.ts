9ca4f91046abd3dd22870630c89051eacc457bc6
selengr <rezaa997karbakhsh@gmail.com>
2026-09-13 20:12:31 +0330

    plugged snake into the main router

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { renderHub } from "./hub/hub";
 import { renderTicTacToe } from "./games/tictactoe/view";
 import { renderRps } from "./games/rps/view";
 import { renderMemory } from "./games/memory/view";
+import { renderSnake } from "./games/snake/view";
 import { parseRoute } from "./shared/router";
 
 const rootEl = document.querySelector<HTMLDivElement>("#app");
@@ -11,9 +12,18 @@ if (!rootEl) {
 }
 const app: HTMLElement = rootEl;
 
+type CleanupHost = HTMLElement & { __snakeCleanup?: () => void };
+
+function cleanup(): void {
+  (app as CleanupHost).__snakeCleanup?.();
+  (app as CleanupHost).__snakeCleanup = undefined;
+}
+
 function render(): void {
+  cleanup();
   const route = parseRoute(window.location.hash);
-  if (route === "tictactoe") renderTicTacToe(app);
+  if (route === "snake") renderSnake(app);
+  else if (route === "tictactoe") renderTicTacToe(app);
   else if (route === "rps") renderRps(app);
   else if (route === "memory") renderMemory(app);
   else renderHub(app);
